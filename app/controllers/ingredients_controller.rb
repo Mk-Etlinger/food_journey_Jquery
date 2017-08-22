eb0a14ce657e685c3f9bb6c53671d4e239245a3b
ETphonehomeslice <mk.etlinger@gmail.com>
2017-08-22 14:36:19 -0400

    rendered layout: false

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -4,18 +4,19 @@ class IngredientsController < ApplicationController
     user_signed_in?
     authenticate_user!
     @meal = Meal.find_by(id: params[:meal_id])
+    render 'ingredients/index', layout: false
   end
 
-  def safe
-    @ingredient = Ingredient.new
-  end
+  # def safe
+  #   @ingredient = Ingredient.new
+  # end
 
-  def mark_safe
-    safe_ingredients = ingredient_params[:id]
-    @ingredients = Ingredient.where(id: safe_ingredients)
-    @ingredients.each { |ingredient| ingredient.update(safe: true) }
-    redirect_to dashboard_path
-  end
+  # def mark_safe
+  #   safe_ingredients = ingredient_params[:id]
+  #   @ingredients = Ingredient.where(id: safe_ingredients)
+  #   @ingredients.each { |ingredient| ingredient.update(safe: true) }
+  #   redirect_to dashboard_path
+  # end
 
   private
 
