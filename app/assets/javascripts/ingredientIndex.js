51c81af3856e41b2fc0f9c7159f46e548a754d96
ETphonehomeslice <mk.etlinger@gmail.com>
2017-08-23 16:58:01 -0400

    Updated to use $.get() and is now rendering a JSON response

--- app/assets/javascripts/ingredientIndex.js
+++ app/assets/javascripts/ingredientIndex.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
     $('.meal_link').on('click', 'a', function(event) {
             event.preventDefault()
-            var action = $(this).attr('href')
-            var mealLink = $(this).parent()
-            $.ajax(action, {
-                context: mealLink,
-                success: function(response) {
-                    mealLink.append(`<p>${response}</p>`)
-                }     
-            })
+            var ul = $(this).parent().find('ul')
+            ul.html("")
+            $.get(this.href, function(json) {
+                let content = ''
+                json.ingredients.forEach(function(ingredient) {
+                    content += `<li>${ingredient.name}</li>`
+                })
+                ul.append(`${content}`)
+            })           
         });
 });
