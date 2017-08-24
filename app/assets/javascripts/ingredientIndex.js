f1c96d8bd790c628dd9ed1ffdc05578a89e23692
Mike Etlinger <mk-etlinger@gmail.com>
2017-08-24 16:24:23 -0400

    updated the event to accommodate ajax request loading a new meal and updated to use jsonResponseHandler

--- app/assets/javascripts/ingredientIndex.js
+++ app/assets/javascripts/ingredientIndex.js
@@ -1,13 +1,10 @@
 $(document).ready(function() {
-    $('.meal_link').on('click', 'a', function(event) {
+    $(document).on('click', '.meal_link a', function(event) {
             event.preventDefault()
             var ul = $(this).parent().find('ul')
             $.get(this.href, function(json) {
-                let content = ''
-                json.ingredients.forEach(function(ingredient) {
-                    content += `<li>${ingredient.name}</li>`
-                })
-                ul.html(`${content}`)
+                let data = new jsonResponseHandler(json.ingredients, this)
+                ul.html(`${data.displayIngredientIndex()}`)
             })           
         });
 });
