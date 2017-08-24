472e025c767272fe91ace90ec627091923d1b22f
Mike Etlinger <mk-etlinger@gmail.com>
2017-08-24 11:11:25 -0400

    refactored click event to replace the html with content instead of append()

--- app/assets/javascripts/ingredientIndex.js
+++ app/assets/javascripts/ingredientIndex.js
@@ -2,13 +2,12 @@ $(document).ready(function() {
     $('.meal_link').on('click', 'a', function(event) {
             event.preventDefault()
             var ul = $(this).parent().find('ul')
-            ul.html("")
             $.get(this.href, function(json) {
                 let content = ''
                 json.ingredients.forEach(function(ingredient) {
                     content += `<li>${ingredient.name}</li>`
                 })
-                ul.append(`${content}`)
+                ul.html(`${content}`)
             })           
         });
 });
