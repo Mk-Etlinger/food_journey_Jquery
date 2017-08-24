7a6d80545d38975a5e43d1d10182200edd18c749
Mike Etlinger <mk-etlinger@gmail.com>
2017-08-24 16:23:24 -0400

    added the jsonResponseHandler to display the meal url

--- app/assets/javascripts/createMeal.js
+++ app/assets/javascripts/createMeal.js
@@ -2,14 +2,11 @@ $(document).ready(function() {
     $('#mealForm').on('submit', 'form', function(event) {
         event.preventDefault()
         $.post(this.action, $(this).serialize(), function(meal) {
+            let data = new jsonResponseHandler(meal, this)
             $('#mealForm').html('<p>You successfully created a new meal!</p>')
-            $('#dash_box').before(`<div id="dash_box">
-                <h3 class="meal_link">
-                    <a href=${this.url}/${meal.id}>${meal.meal_type + '-' + meal.description}</a>
-                    <ul id="ingredientList"></ul
-                </h3>
-            </div>`)
-            
+            $('#dash_box').before('<div id="dash_box"> <h3 class="meal_link">' +
+                    data.displayMealUrl() +
+                    '<ul id="ingredientList"> </ul </h3> </div>')            
         })
     });
 });
